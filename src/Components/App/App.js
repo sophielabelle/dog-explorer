b160c5e48641350eb23476a6d33cf20f618f7b25
Alexys Jordan <alexyskjordan@gmail.com>
2023-04-06 18:06:08 -0600

    Add jsx to components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { Home } from '../Home/Home';
+import { Nav } from '../Nav/Nav';
+import { fetchData } from '../../data/apiCall';
 
 function App() {
-
+  return (
+    <div>
+      <Nav />
+      <Home />
+    </div>
+  )
 }
 
 export default App;
