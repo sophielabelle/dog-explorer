c18a571bfbc8cf6b6c342efb76f61d4c482471f8
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-14 08:31:38 -0700

    Update fixture and intercept

--- cypress/e2e/BreedPage.cy.js
+++ cypress/e2e/BreedPage.cy.js
@@ -2,13 +2,15 @@ describe('Single Breed Page', () => {
   it('Should be able to click any exhibit card to be taken to that page', () => {
     cy.visit('http://localhost:3000/dogexhibits')
     cy.get('#bulldog > img').click()
-    cy.intercept('GET', 'https://dog.ceo/api/breed/bulldog/images', {
-      statusCode: 200, 
-      fixture:  singleExhibitBreeds.json,
+    cy.intercept("GET", "https://dog.ceo/api/breed/bulldog/images", {
+      statusCode: 200,
+      fixture: singleExhibitBreeds.json
     })
+    .visit("http://localhost:3000/dogexhibits/Bulldog")
   })
   
   it('Should have the name of a dog breed', () => {
- 
+    // cy.get(".dog-card")
+
   })
 })
