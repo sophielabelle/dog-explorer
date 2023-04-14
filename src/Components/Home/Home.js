897844532fea50e235f44496213c72e301046b65
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-13 19:38:01 -0700

    Update css styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,6 @@ import React, {useEffect, useState} from "react";
 import { fetchData } from "../../data/apiCall";
 import './Home.css'
 
-
-
 export const Home = () => {
   const [randomDog, setRandomDog] = useState('');
   const [error, setError] = useState('');
@@ -27,12 +25,12 @@ export const Home = () => {
       <div className="home-contents-container">
         <div className="about-us">
           <p>Hello! Stay on this page to find adorable random dogs. Or visit our exhibits for a specific dog breed where you can save your favorites for later.</p>
-         </div>
+        </div>
         <div className="get-random">
           <button onClick={() => fetchRandomDog()} >Get Random Dog!</button>
           {error && <p>There was a {error} error, please try again later.</p>}
           <div className="img-container">
-            <img className="random-dog" src={randomDog}/>
+            <img className="random-dog-img" src={randomDog}/>
           </div>
         </div>  
       </div>
