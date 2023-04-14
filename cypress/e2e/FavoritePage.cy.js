c01bdfb034e966a377ca82ea9475fc9e1e4d9d44
Travis Parks <travisparks0788@gmail.com>
2023-04-14 19:20:02 -0400

    Add tests for favorite page

--- cypress/e2e/FavoritePage.cy.js
+++ cypress/e2e/FavoritePage.cy.js
@@ -1,57 +1,31 @@
-describe('Exhibit Page', () => {
-  // beforeEach(() => {
-  //   cy.visit('http://localhost:3000/favorites')
-  //   })
-  
-    it('should have no favorites when first visiting the site', () => {
+describe('Favorite Page', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://dog.ceo/api/breed/labrador/images", {
+      statusCode: 200,
+      fixture: "singleExhibitBreeds.json"
+    })
+    cy.visit('http://localhost:3000/dogexhibits')
+    cy.get('#labrador > img').click()
+  })
+
+
+  it('should have no favorites when first visiting the site', () => {
       cy.visit('http://localhost:3000/favorites')
-      cy.get(".dog-card").should('have.length', 0)
-      //input message when no dogs are there
+      cy.get(".fave-dog").should('have.length', 0)
+      cy.get(".fave-message").contains("You haven't favorited any dogs yet...")
     })
 
-    it('should allow a user to favorite a dog image', () => {
+    it('should allow a user to favorite a dog image and delete one', () => {
       cy.visit('http://localhost:3000/dogexhibits')
-      cy.get('#bulldog > img').click()
+      cy.get('#labrador > img').click()
       cy.get(':nth-child(1) > .favorite-button').click()
       cy.get(':nth-child(2) > .favorite-button').click()
       cy.get('[href="/favorites"] > .nav-btn').click()
-      cy.get(".dog-card").should('have.length', 2)
-      //check the two images are there
-     
-
-    })
-
-    it.only('should be able to delete a dog from the favorite page', () => {
-      cy.visit('http://localhost:3000/dogexhibits')
-      cy.get('#bulldog > img').click()
-      cy.get(':nth-child(1) > .favorite-button').click()
-      cy.get(':nth-child(2) > .favorite-button').click()
-      cy.get('[href="/favorites"] > .nav-btn').click()
-
+      cy.get(".fave-dog").should('have.length', 2)
       cy.url().should('includes', '/favorites')
       cy.get(':nth-child(1) > .delete-button').click()
-      cy.get(".dog-card").should('have.length', 1)
+      cy.get(".fave-dog").should('have.length', 1)
       cy.get(':nth-child(1) > .delete-button').click()
-      cy.get(".dog-card").should('have.length', 0)
-      
+      cy.get(".fave-dog").should('have.length', 0)
     })
-
-
 })
-
-//no favorites
-
-//
-
-
-// it('should be able to favorite different dogs', () => {
-//   cy.get('[href="/dogexhibits"] > .nav-btn').click()
-//   cy.get('#schnauzer > img').click()
-//   cy.get(':nth-child(1) > .favorite-button').click()
-//   cy.get(':nth-child(2) > .favorite-button').click()
-//   cy.get('[href="/favorites"] > .nav-btn').click()
-//   cy.get('.dog-card').should('have.attr', 'src')
-//   cy.get('.dog-card').should('have.length', 2);
-//   cy.get(':nth-child(1) > .delete-button').click()
-//   cy.get('.dog-card').should('have.length', 1);
-// })
