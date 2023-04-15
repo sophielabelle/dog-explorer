3dc602cd0469fe000c31ec867d97e73d76a0403b
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-15 07:24:44 -0700

    Update single dog breed page testing to include modal view

--- cypress/e2e/BreedPage.cy.js
+++ cypress/e2e/BreedPage.cy.js
@@ -9,7 +9,7 @@ describe('Single Breed Page', () => {
   })
 
   it('Should have the specific dog breed cards displayed', () => {
-    cy.get(".dog-card").should('have.length', 6)
+    cy.get(".dog-card-small").should('have.length', 6)
   })
   
   it('Should have the images and buttons for each card', () => {
@@ -17,5 +17,10 @@ describe('Single Breed Page', () => {
     cy.get('.breed-page > :nth-child(1)').find('.favorite-button')
     cy.get('.breed-page > :nth-child(6)').find('img').should('have.attr', 'src', 'https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_1157.jpg')
     cy.get('.breed-page > :nth-child(6)').find('.favorite-button')
+  })
+
+  it('Should open a single large image when an image is clicked', () => {
+    cy.get(':nth-child(1) > div > .dog-card-small').click()
+    cy.get('.dog-card-large').should('have.length', 1).click().should('have.length', 0)
   })
 })
