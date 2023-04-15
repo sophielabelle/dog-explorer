cb8a1f1e1c06504c278671f4318179a897fdf249
Alexys Jordan <alexyskjordan@gmail.com>
2023-04-14 18:49:25 -0600

    Add Modal to the breed page

--- src/Components/BreedCard/BreedCard.js
+++ src/Components/BreedCard/BreedCard.js
@@ -7,25 +7,57 @@ export const BreedCard = ({url}) => {
   const { addFavs } = useContext(DogContext);
   const [favorites, setFavorites] = addFavs;
   const [disabled, setDisabled] = useState(false)
-
+  const [isOpen, setIsOpen] = useState(false)
+  let display
   const favoriteDog = (newFav) => {
     setFavorites([...favorites, newFav]);
   }
 
-  const showModal = () => {
-    return (
-    <Modal url={url}/>
-    )
+  const handleShowDialog = () => {
+    setIsOpen(true)
+    console.log('clicked')
+    console.log(url)
+     display = isOpen && <Modal  url={url} />
   }
 
 
   return (
     <div className="card">
-      <img onClick={() => showModal()}className="dog-card" src={url} alt=""/>
+      <img className="dog-card" src={url} alt="" onClick={handleShowDialog}/>
       <button onClick={() => {
         favoriteDog(url) 
         setDisabled(true)
         }} className="favorite-button" disabled={disabled}>Save</button>
+        {display}
     </div>
   );
 }
+
+// render() {
+//   return (
+//     <div>
+//       <img
+//         className="small"
+//         src="/Anj.png"
+//         onClick={this.handleShowDialog}
+//         alt="no image"
+//       />
+//       {this.state.isOpen && (
+//         <dialog
+//           className="dialog"
+//           style={{ position: 'absolute' }}
+//           open
+//           onClick={this.handleShowDialog}
+//         >
+//           <img
+//             className="image"
+//             src="/Anj.png"
+//             onClick={this.handleShowDialog}
+//             alt="no image"
+//           />
+//         </dialog>
+//       )}
+//     </div>
+//   );
+// }
+// }
--- src/Components/BreedCard/ImageModal/ImageModal.js
+++ src/Components/BreedCard/ImageModal/ImageModal.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./ImageModal.css";
 
 export const Modal = ({ url }) => {
+
   return (
+
+  <dialog
+    className="dialog"
+    style={{ position: 'absolute' }}
+    open
+  >
+    <img
+      className="image"
+      src={url}
+      alt="no image"
+    />
+  </dialog>
+
     // <div class="modal-body">
     //   <img className="modal-image "src={url} alt="dog"/>
     // </div>
