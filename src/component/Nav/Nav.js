488fb44a85f8c7fa105ec41cc8d9d4e691f301d5
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-17 13:25:08 -0700

    Dynamically display the nav buttons based on url

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Nav.css";
 
 export const Nav = () => {
+  const location = useLocation();
+  const showHome = location.pathname !== "/" ? <button className='nav-btn'>Home</button> : <button className='nav-btn hidden'>Home</button>
+  const showFavorites  = location.pathname !== "/favorites" ? <button className='nav-btn'>Favorite Dogs</button> : <button className='nav-btn  hidden'>Favorite Dogs</button>
+  const showBreeds = location.pathname !== "/dogexhibits" ? <button className='nav-btn'>Choose Your Dog Breed</button> : <button className='nav-btn hidden'>Choose Your Dog Breed</button>
+
   return (
     <header className="nav-bar">
       <NavLink to="/" style={{ textDecoration: 'none', color: 'white' }}>
@@ -10,13 +15,13 @@ export const Nav = () => {
       </NavLink>
       <div className="nav-btns">
         <NavLink to="/">
-          <button className='nav-btn'>Home</button>
+          {showHome}
         </NavLink>
         <NavLink to="/dogexhibits">
-          <button className='nav-btn'>Choose Your Dog Breed</button>
+          {showBreeds}
         </NavLink>
         <NavLink to="/favorites" >
-          <button className='nav-btn'>Favorite Dogs</button>
+          {showFavorites}
         </NavLink>
       </div>
     </header>
